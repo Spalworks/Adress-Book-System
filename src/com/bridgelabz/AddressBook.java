46952ca6c3cd24c73ab20163f94586e2828b9231
Sourav Pal <spalworks@gmail.com>
2023-01-13 08:08:26 +0530

    added: Create a contact

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,46 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook {
+	
+/* Taking arraylist to save the contacts */	
+	 ArrayList<Contact> addressbook = new ArrayList<>();
+	 
+	 static Scanner sc = new Scanner(System.in);
+	 
+ /*Creating contact with this method */
+	 void addContactDetails() {
+		Contact contact = new Contact();
+
+		System.out.println("Enter Firstname : ");
+		contact.setFirstName(sc.next());
+
+		System.out.println("Enter Lastname : ");
+		contact.setLastName(sc.next());
+
+		System.out.println("Enter Address : ");
+		contact.setAddress(sc.next());
+
+		System.out.println("Enter City : ");
+		contact.setCity(sc.next());
+
+		System.out.println("Enter State : ");
+		contact.setState(sc.next());
+
+		System.out.println("Enter ZIP : ");
+		contact.setZip(sc.next());
+
+		System.out.println("Enter PhoneNumber : ");
+		contact.setPhoneNumber(sc.next());
+
+		System.out.println("Enter Email : ");
+		contact.setEmail(sc.next()); 
+
+		addressbook.add(contact);		//storing the contact (details) in arraylist
+
+		System.out.println("Contact added successfully.\n");
+		System.out.println(addressbook);
+	}
+}
